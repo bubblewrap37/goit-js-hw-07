2b2e0597629f942883dc59b74fbce43c841b89eb
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-24 01:57:51 +0200

    task 1 update
    added escape key to modal and removed lighbox creation on div container

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,18 @@ const gallery = document.querySelector("div.gallery");
 
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
+
+  if (event.target.nodeName === "DIV") {
+    return;
+  }
+
   const fullsize = event.target.dataset.source;
   const instance = basicLightbox.create(`<img src="${fullsize}" />`);
   instance.show();
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
 });
