70ef4de3e17b2af16985b983b84d7c5aa7a1e78d
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-24 02:32:54 +0200

    task 2 initial

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(function (item) {
+    const img = `<li><a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt=${item.description} />
+</a></li>
+`;
+    return img;
+  })
+  .join("");
+
+const gallery = document.querySelector("ul.gallery");
+gallery.innerHTML = markup;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const instance = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
